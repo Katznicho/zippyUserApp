4043f7395fa8ada5e077f1d5c998c07ca38166dc
katznicho <katznicho@gmail.com>
2025-01-16 20:42:50 +0300

    Reapply attributes for binary files

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,8 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// metro.config.js
+const {
+    wrapWithReanimatedMetroConfig,
+  } = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
@@ -9,3 +13,7 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+
+  
+  module.exports = wrapWithReanimatedMetroConfig(config);
